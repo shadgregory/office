7eb64630e2d3222b608c7d9fcb081e5ce56f6c93
Shad Gregory <Shad.Gregory@austintexas.gov>
2022-04-24 14:18:09 -0500

    upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring/ring-core "1.7.1"]
-                 [org.apache.poi/poi "4.1.0"]
-                 [org.apache.poi/poi-ooxml "4.1.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "1.9.5"]
+                 [org.apache.poi/poi "4.1.2"]
+                 [org.apache.poi/poi-ooxml "4.1.2"]]
   :main ^:skip-aot office.core
+  :plugins [[lein-ancient "1.0.0-RC3"]]
   :local-repo "local-m2"
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
